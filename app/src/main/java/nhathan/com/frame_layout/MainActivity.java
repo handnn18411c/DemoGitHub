1bded863dd485df4965c2016cd9e45126c5a08b2
NhatHan <handnn18411c>
2020-11-15 21:52:21 +0700

    Huy, upload Lan2, 15/11

--- app/src/main/java/nhathan/com/frame_layout/MainActivity.java
+++ app/src/main/java/nhathan/com/frame_layout/MainActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-//VuQUangHuy
+    //VuQUangHuy
+    //Trưởng ban học thuật ITB
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
